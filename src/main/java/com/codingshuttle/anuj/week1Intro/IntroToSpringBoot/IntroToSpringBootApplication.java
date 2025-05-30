4298fd1dc5d362eb85270e9d6aa8aeb4aefca293
harish881 <mailharishdeshpande@gmail.com>
2025-05-30 13:23:55 +0530

    Inital Commit

--- src/main/java/com/codingshuttle/anuj/week1Intro/IntroToSpringBoot/IntroToSpringBootApplication.java
+++ src/main/java/com/codingshuttle/anuj/week1Intro/IntroToSpringBoot/IntroToSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IntroToSpringBootApplication implements CommandLineRunner {
-	@Autowired
+	@Autowiredconfiggloba user
 	Apple obj ;
 	public static void main(String[] args) {
 		SpringApplication.run(IntroToSpringBootApplication.class, args);
