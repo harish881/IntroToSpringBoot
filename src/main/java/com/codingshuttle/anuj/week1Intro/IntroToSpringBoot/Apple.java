d48348d63fc12e7d9c569228051f0b9162ce6cbb
Harish Deshpande <harishdeshpande@Harishs-MacBook-Air.local>
2025-05-30 12:52:13 +0530

    first component

---
+++ src/main/java/com/codingshuttle/anuj/week1Intro/IntroToSpringBoot/Apple.java
@@ -0,0 +1,11 @@
+package com.codingshuttle.anuj.week1Intro.IntroToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+
+    void eatApple() {
+        System.out.println("Eating apple");
+    }
+}
--- src/main/java/com/codingshuttle/anuj/week1Intro/IntroToSpringBoot/IntroToSpringBootApplication.java
+++ src/main/java/com/codingshuttle/anuj/week1Intro/IntroToSpringBoot/IntroToSpringBootApplication.java
@@ -1,13 +1,23 @@
 package com.codingshuttle.anuj.week1Intro.IntroToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroToSpringBootApplication {
-
+public class IntroToSpringBootApplication implements CommandLineRunner {
+	@Autowired
+	Apple obj ;
 	public static void main(String[] args) {
 		SpringApplication.run(IntroToSpringBootApplication.class, args);
+
+
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		obj.eatApple();
+	}
 }
